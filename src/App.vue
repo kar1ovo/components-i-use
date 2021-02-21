665dbb1cbedc4a02a198e8c65935cae525cc9e2f
kyleorz213 <2395931623@qq.com>
2021-02-21 18:01:40 +0800

    :construction: article info component in progress

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <template>
+  <ArticleInfo
+    title="回顾2020，程序员转型的起步"
+    summary="对未来的焦虑应该是每个程序员都能体会到的吧，面对招聘的年龄门槛、工作与生活的平衡、不良的生活习惯，以后的路要怎么走充满了不确定性。但是又因为程序员是技术工种，自己所掌握的技能可以从接外包、作产品、写博客、录视频等多方面进行输出，看似又不缺出路，只是怀疑能不能做好，所以一边担心自己能不能转型或改行，一边又极其期望逃离没有出头之日的 996 ，这是一个难题。"
+    eyes="996"
+    createTime="2000.1.9"
+  />
+  <ArticleInfo
+    title="整理了5个JavaScript怪异行为及其原因"
+    summary="如果你用 JavaScript 写过项目或者参加过面试，那一定遇到过不少令人匪夷所思的问题。JavaScript 早期的规范不统一，也没有严格的标准，再加上它的语法灵活多样，有些看起来就不正确的代码却能正常执行，一些看起来符合逻辑的代码，运行结果却相差十万八千里。这些问题在日常开发中经常会导致 BUG，更重要的是，很多面试官会把它们拿出来当考验咱们 JS 工程师的能力。那么这篇文章就总结了 5 个 JavaScript 比较坑的问题，以及它们出现的原因和解决方法。"
+    eyes="007"
+    createTime="2000.2.13"
+  />
 </template>
 
 <script setup>
-
+import ArticleInfo from "./components/ArticleInfo.vue";
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
 </script>
 
-<style>
-</style>
+<style></style>
---
+++ src/components/ArticleInfo.vue
@@ -0,0 +1,102 @@
+<template>
+  <div
+    class="max-w-sm mx-auto my-8 p-8 space-y-4 font-sans bg-gray-100 rounded-2xl sm:max-w-4xl shadow-2xl"
+  >
+    <div>
+      <p class="text-3xl font-bold">{{ title }}</p>
+    </div>
+    <div class="grid grid-cols-12 gap-x-2 opacity-50">
+      <svg
+        class="col-start-1 col-span-1 h-6"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
+        />
+      </svg>
+      <button
+        class="bg-blue-500 text-white rounded-md px-2 text-sm col-start-2 col-span-4 h-6"
+      >
+        {{ createTime }}
+      </button>
+      <svg
+        class="col-start-6 col-span-1 h-6"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z"
+        />
+      </svg>
+      <button
+        class="bg-blue-500 text-white rounded-md px-2 text-sm col-start-7 col-span-2 h-6"
+      >
+        个人
+      </button>
+      <button
+        class="bg-blue-500 text-white rounded-md px-2 text-sm col-start-9 col-span-2 h-6"
+      >
+        经验
+      </button>
+    </div>
+    <div>
+      <p class="font-light leading-7">
+        {{ summary }}
+      </p>
+    </div>
+    <div class="grid grid-cols-12 h-6">
+      <svg
+        class="col-start-1 col-span-1 h-8"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+        />
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+        />
+      </svg>
+      <div class="col-start-3 col-span-1 h-8 text-2xl">{{ eyes }}</div>
+      <button
+        class="bg-blue-500 text-white rounded-lg px-2 text-base shadow-2xl col-start-9 col-span-4 h-8 focus:outline-none"
+      >
+        阅读原文
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["title", "summary", "eyes", "createTime", "tags"],
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+span {
+  display: inline-block;
+}
+</style>
